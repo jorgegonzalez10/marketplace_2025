87b5bc6221d98fac81e4b23669971edeed8e0a43
jorge gonzalez <gonzalezjor13@gmail.com>
2025-09-03 23:12:46 -0500

    creacion de token

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,9 +11,9 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: 201
+      render json: @user, status: :created
     else
-      render json: { message: "Error al crear el usuario" }, status: 422
+      render json: { message: "Error al crear el usuario" }, status: :unprocessable_entity
     end
   end
 
@@ -33,7 +33,7 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password_digest)
+    params.require(:user).permit(:email, :password)
   end
 
   def set_user
